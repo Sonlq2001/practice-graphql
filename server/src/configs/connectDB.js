aa02e6c7dd3376c2f04ac4e2e39fd4339585b989
Sonlq2001 <sonweb2001@gmail.com>
2022-04-13 20:29:35 +0700

    config

--- server/src/configs/connectDB.js
+++ server/src/configs/connectDB.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		const api =
-			"mongodb+srv://restaurant:restaurant@cluster0.qzrxu.mongodb.net/restaurant?retryWrites=true&w=majority";
+		const api = process.env.DATABASE_URL;
 		await mongoose.connect(api, {});
 
 		console.log("Connect db successfully");
